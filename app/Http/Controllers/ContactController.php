43d6247baa762a4e3dacf4362472cc3908acca01
Felipe Kauãn <felipekauan30@outlook.com>
2026-05-20 20:42:05 -0300

    feat: create ContactController with CRUD and search

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    public function store(Request $request)
+    {
+        $contact = Contact::create([
+            'name' => $request->input('name'),
+            'phone' => $request->input('phone'),
+            'email' => $request->input('email'),
+            'category' => $request->input('category'),
+        ]);
+
+        return response()->json([
+            'message' => 'Contato criado com sucesso!',
+            'contact' => $contact,
+        ], 201);
+    }
+
+    public function index(Request $request)
+    {
+        $search = $request->query('search');
+
+        $contacts = Contact::when($search, function($query) use ($search) {
+            $query->where('name', 'LIKE', '%' . $search . '%');
+        })->get();
+
+        return response()->json([
+            'contacts' => $contacts,
+        ]);
+    }
+
+    public function update(Request $request, Contact $contact)
+    {
+        $contact->update([
+            'name' => $request->input('name'),
+            'phone' => $request->input('phone'),
+            'email' => $request->input('email'),
+            'category' => $request->input('category'),
+        ]);
+
+        return response()->json([
+            'message' => 'Contato alterado com sucesso!',
+            'contato' => $contact,
+        ]);
+    }
+
+    public function destroy(Contact $contact)
+    {
+        $contact->delete();
+
+        return response()->json([
+            'message' => 'Contato apagado com sucesso!',
+        ]);
+    }
+}
